be523504843f7638ade0ffb490814984e5c238ca
Ben Hughes <ben@goincremental.com>
2017-06-28 12:56:41 +0200

    Updates

--- project/api/views.py
+++ project/api/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, jsonify, request, make_response
+from flask import Blueprint, jsonify, request, make_response, render_template
 from sqlalchemy import exc
 
 from project.api.models import User
 from project import db
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
 
 @users_blueprint.route('/ping', methods=['GET'])
@@ -13,6 +13,18 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+    users = User.query.order_by(User.created_at.desc()).all()
+    return render_template('index.html', users=users)
+
+
 
 @users_blueprint.route('/users/<user_id>', methods=['GET'])
 def get_single_user(user_id):
