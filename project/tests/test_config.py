d24f23427151d55a8f669edb2b50124ab9387f15
Ben Hughes <ben@goincremental.com>
2017-06-28 13:58:43 +0200

    Added stuff

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from flask import current_app
@@ -19,7 +20,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertFalse(current_app is None)
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_dev'
+            os.environ.get('DATABASE_URL')
         )
 
 
@@ -35,7 +36,7 @@ class TestTestingConfig(TestCase):
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_test'
+            os.environ.get('DATABASE_URL')
         )
 
 
@@ -48,6 +49,10 @@ class TestProductionConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] is 'my_precious')
         self.assertFalse(app.config['DEBUG'])
         self.assertFalse(app.config['TESTING'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            os.environ.get('DATABASE_URL')
+        )
 
 
 if __name__ == '__main__':
